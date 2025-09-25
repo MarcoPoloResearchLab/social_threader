5a9aa546f67387a3f54f8ddf6c7538aff91f3d20
Vadym Tyemirov <temirov@users.noreply.github.com>
2025-09-25 13:28:36 -0700

    Configure workflow for main branch and Chromium project

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,21 @@
-const { defineConfig } = require("@playwright/test");
+const { defineConfig, devices } = require("@playwright/test");
 
 const PLAYWRIGHT_TEST_DIRECTORY = "playwright";
+const CHROMIUM_PROJECT = devices["Desktop Chrome"];
 
 module.exports = defineConfig({
     testDir: PLAYWRIGHT_TEST_DIRECTORY,
     retries: process.env.CI ? 2 : 0,
     use: {
         headless: true
-    }
+    },
+    projects: [
+        {
+            name: CHROMIUM_PROJECT.name,
+            use: {
+                ...CHROMIUM_PROJECT,
+                headless: true
+            }
+        }
+    ]
 });
